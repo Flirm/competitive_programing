7ffa93f7b28fd95b5e494b36fd0ee1a028c5fb9a
Francisco de Lima Ramos <121270101+Flirm@users.noreply.github.com>
2025-10-08 22:28:58 -0300

    Create 1007.cpp

---
+++ beecrowd/1007.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+int main() 
+{
+    using namespace std;
+    
+    int a, b, c, d, diff;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    diff = (a*b - c*d);
+    cout << "DIFERENCA = " << diff << endl;
+ 
+    return 0;
+}

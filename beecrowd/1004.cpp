773daa5dfc6b5b72415d42ab53e011d052cd2abb
Francisco de Lima Ramos <121270101+Flirm@users.noreply.github.com>
2025-10-08 22:28:51 -0300

    Create 1004.cpp

---
+++ beecrowd/1004.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+int main() 
+{
+    using namespace std;
+    
+    int a, b, prod;
+    cin >> a;
+    cin >> b;
+    prod = a * b;
+    cout << "PROD = " << prod << endl;
+ 
+    return 0;
+}

f57a59c19033343e4d0da47feda6483a8a190ad1
Francisco de Lima Ramos <121270101+Flirm@users.noreply.github.com>
2025-10-22 13:10:44 -0300

    Create 12_integer_to_roman.cpp

---
+++ leetcode/medium/12_integer_to_roman.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string intToRoman(int num) {\
+        string res = "";
+        vector<int> n = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        vector<string> s = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        for(int i = 0; i < n.size(); i++){
+            while(num >= n[i]){
+                res += s[i];
+                num -= n[i];
+            }
+        }
+        return res;
+    }
+};

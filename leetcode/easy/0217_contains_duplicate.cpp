4a52187799b99167edb3f944921760294d5fd720
Francisco de Lima Ramos <121270101+Flirm@users.noreply.github.com>
2025-10-23 20:24:00 -0300

    Create 0217_contains_duplicate.cpp

---
+++ leetcode/easy/0217_contains_duplicate.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,bool> m;
+        for(int i = 0; i < nums.size(); i++){
+            if(m[nums[i]]) return true;
+            else m[nums[i]] = true;
+        }
+        return false;
+    }
+};

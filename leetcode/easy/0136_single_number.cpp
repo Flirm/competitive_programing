008346ca6620a750434094014232315a6577df80
Francisco de Lima Ramos <121270101+Flirm@users.noreply.github.com>
2025-10-25 19:51:47 -0300

    Create 0136_single_number.cpp

---
+++ leetcode/easy/0136_single_number.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for(auto n : nums){
+            res ^= n;
+        }
+        return res;
+    }
+};

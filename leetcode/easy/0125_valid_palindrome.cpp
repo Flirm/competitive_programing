4c2fe78b559d507df9c3537820bbc187cbf96c7d
Francisco de Lima Ramos <121270101+Flirm@users.noreply.github.com>
2025-10-25 23:09:07 -0300

    Create 0125_valid_palindrome.cpp

---
+++ leetcode/easy/0125_valid_palindrome.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0, r = s.size()-1;
+        while(l<r){
+            if(!isalnum(s[l])) {l++; continue;}
+            if(!isalnum(s[r])) {r--; continue;}
+            if(tolower(s[l]) != tolower(s[r])) return false;
+            l++;r--;
+        }
+        return true;
+    }
+};

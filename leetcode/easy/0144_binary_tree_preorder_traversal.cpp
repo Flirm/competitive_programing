c207e3e93ea9cc7142cf0f511300278b631690c3
Francisco de Lima Ramos <121270101+Flirm@users.noreply.github.com>
2025-10-27 01:26:26 -0300

    Create 0144_binary_tree_preorder_traversal.cpp

---
+++ leetcode/easy/0144_binary_tree_preorder_traversal.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void auxTrav(TreeNode* root, vector<int> *v){
+        if(!root) return;
+        v->push_back(root->val);
+        auxTrav(root->left, v);
+        auxTrav(root->right, v);
+    }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> v;
+        auxTrav(root, &v);
+        return v;
+    }
+};

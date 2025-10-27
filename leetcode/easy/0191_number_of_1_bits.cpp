c9fbe75665204cdf97ce1e82384d309c8f251054
Francisco de Lima Ramos <121270101+Flirm@users.noreply.github.com>
2025-10-27 20:48:43 -0300

    Create 0191_number_of_1_bits.cpp

---
+++ leetcode/easy/0191_number_of_1_bits.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int c = 0;
+        while(n){
+            c += (n&1);
+            n = n>>1;
+        }
+        return c;
+    }
+};

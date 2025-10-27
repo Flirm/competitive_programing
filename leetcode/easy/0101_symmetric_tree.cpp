771bbb9f6a1da29d81a8231d0fae0642bd31e2b2
Francisco de Lima Ramos <121270101+Flirm@users.noreply.github.com>
2025-10-27 14:03:49 -0300

    Create 0101_symmetric_tree.cpp

---
+++ leetcode/easy/0101_symmetric_tree.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        return symAux(root,root);
+    }
+
+    bool symAux(TreeNode* root1, TreeNode* root2){
+        if(!root1 && !root2) return true;
+        if(!root1 || !root2) return false;
+        if(root1->val != root2->val) return false;
+        return symAux(root1->right, root2->left) && symAux(root1->left, root2->right);
+    }
+};

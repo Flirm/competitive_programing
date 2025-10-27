7588eabed8e642315f2d583828fdab8f4b485167
Francisco de Lima Ramos <121270101+Flirm@users.noreply.github.com>
2025-10-27 01:26:30 -0300

    Create 0121_best_time_to_buy_and_sell_stock.cpp

---
+++ leetcode/easy/0121_best_time_to_buy_and_sell_stock.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int m = INT_MAX;
+        int profit = 0;
+        for(int i = 0; i < prices.size(); i++){
+            if(prices[i] < m){
+                m = prices[i];
+            }
+            profit = max(profit, prices[i]-m);
+        }
+        return profit;
+    }
+};

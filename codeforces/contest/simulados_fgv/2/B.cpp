8cd368aa5e6077aa5c6c5769c0c98b55e891ce87
Francisco de Lima Ramos <121270101+Flirm@users.noreply.github.com>
2026-05-22 19:09:44 -0300

    Create B.cpp

---
+++ codeforces/contest/simulados_fgv/2/B.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+    int a;
+    cin >> a;
+    if(a==45) cout << "Ambos";
+    else if(a>45) cout << "Saad";
+    else cout << "Costa";
+    return 0;
+}

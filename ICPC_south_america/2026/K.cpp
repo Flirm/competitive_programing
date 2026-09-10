74e84ef79ef4d74888fe5cd8aaa6047d29ac113d
Francisco de Lima Ramos <121270101+Flirm@users.noreply.github.com>
2026-09-10 00:40:06 -0300

    Create K.cpp

---
+++ ICPC_south_america/2026/K.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main(){
+    int n, a, min_diff = INT32_MAX;
+    cin >> n;
+    vector<int> pref(n+1);
+
+    for(int i = 1; i <= n; i++){
+        cin >> pref[i];
+        pref[i] = pref[i] + pref[i-1];
+    }
+    for(int i = 1; i <= n; i++){
+        int k; cin >> k;
+        min_diff = min(min_diff, pref[i]-pref[i-1]-k);
+        if(min_diff < 0){
+            cout << "-1\n";
+            return 0;
+        }
+    }
+    cout << pref[n]-min_diff << "\n";
+
+    return 0;
+}

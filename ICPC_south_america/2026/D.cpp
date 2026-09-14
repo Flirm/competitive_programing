1b66efda616412a1832c6a147bf2cf40c566f948
Francisco de Lima Ramos <121270101+Flirm@users.noreply.github.com>
2026-09-14 11:43:47 -0300

    Create D.cpp

---
+++ ICPC_south_america/2026/D.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> map(n, vector<int>(m));
+
+    for(int i = 0; i < n; i++){
+        string s;
+        cin >> s;
+        for(int j = 0; j < m; j++){
+            map[i][j] = (s[j] == '#') ? 0 : 1;
+        }
+    }
+
+    vector<int> s(7,1);
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(!map[i][j]){
+                if(map[n-(i+1)][j]) s[0]=0;
+                if(map[i][m-(j+1)]) s[1]=0;
+                if(map[n-(i+1)][m-(j+1)]) s[2]=0;
+                if(n == m){
+                    if(map[j][i]) s[3]=0;
+                    if(map[n-(j+1)][i]) s[4]=0;
+                    if(map[j][n-(i+1)]) s[5]=0;
+                    if(map[n-(j+1)][n-(i+1)]) s[6]=0;
+                }
+            }
+        }
+    }
+
+    cout << 1 + accumulate(s.begin(), s.end(), 0) - (n==m ? 0 : 4) << "\n";
+
+    return 0;
+}
